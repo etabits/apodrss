e2205711714c45bca9b357abecc85f27ee61e6d3
Hasan Arous <arous.hasan@gmail.com>
2021-12-05 00:53:04 +0200

    extract title, author(s), and description

--- scraper.ts
+++ scraper.ts
@@ -29,6 +29,23 @@ export async function day(fname: string) {
   const pageTitle = document.querySelector('title').textContent.trim()
 
   const [cImg, cTitleCred, _cFooter] = document.querySelectorAll('body > center')
+  // extract title
+  const title = cTitleCred.querySelector('b').textContent.trim();
+  // remove first two b elements, usually the title and the copyright label
+  [...cTitleCred.querySelectorAll('b')].slice(0, 2).forEach(elm => elm.remove())
+  const author = [...cTitleCred.querySelectorAll('a')].map(a => {
+    const authorObj: Record<string, string> = { name: a.textContent.trim() }
+    const href = a.getAttribute('href');
+    if (href.startsWith('mailto:')) {
+      authorObj.email = href.substring(7)
+    } else {
+      try {
+        new URL(href)
+        authorObj.link = href
+      } catch (e) { }
+    }
+    return authorObj
+  })
 
   const date = new Date(parseInt('20' + fname.substring(2, 4)),
     parseInt(fname.substring(4, 6)) - 1,
@@ -39,6 +56,12 @@ export async function day(fname: string) {
   const image = await extractImageUrl(cImg)
   // console.log(fname, image)
   // console.log(image, !!img)
+
+  const content = document.querySelector('body > p');
+  const description = content.textContent.trim()
+    .replace(/^Explanation: /, '')
+    .split('.').shift()
+
   return {
     pageTitle,
     date,
